6b9f43fe5cafd34ada2db4f6d47f9321fcedb184
Maeva Montmasson <maevamontmasson365@gmail.com>
2021-08-02 13:44:51 +0200

    ajout des plugin fontawesome et navbarBootstrap

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,19 @@ import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import router from './router'
 import store from './store'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { NavbarPlugin } from 'bootstrap-vue'
+
+Vue.use(NavbarPlugin)
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+library.add(faUserSecret)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
