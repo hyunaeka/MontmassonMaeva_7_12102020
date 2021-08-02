5fe26d8266c08df80f7543f4e04ae35476e22297
Maeva Montmasson <maevamontmasson365@gmail.com>
2021-08-02 14:03:57 +0200

    début du design profil et modif mineur

--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -1,14 +1,25 @@
 <template>
-  <div class="card">
-    <h1 class="card__title">Espace Perso</h1>
-    <p class="card__subtitle">Voilà donc qui je suis...</p>
-    <p>{{user.username}} {{user.bio}} {{user.email}}</p>
+
+  <div class="container d-flex flex-row mt-5 ">
+
+    <div class=" card left-column col-6 mt-5 p-4">
+    <h1 class="card__title">Bienvenue {{user.username}}</h1>
+    <p class="card__subtitle">Informations personnelles</p>
+    <p class=""> Votre pseudo: {{user.username}}  </p>
+    <p>Votre bio: {{user.bio}}</p>
+    <p>Votre email: {{user.email}}</p>
     <img :src="user.photo"/>
-    <div class="form-row">
-      <button @click="logout()" class="button">
-        Déconnexion
-      </button>
+     <button @click="logout()" class="button"> Déconnexion </button>
     </div>
+
+    <div class="card right-column col-6 p-5 mt-5 p-1">
+      <p>Modifier mes <button> informations personnelles </button></p>
+       <p>Changer mon <button> mot de passe </button></p>
+    </div>
+    <div class="form-row">
+      
+    </div>
+
   </div>
 </template>
 
@@ -19,6 +30,13 @@ import { mapState } from 'vuex'
 export default {
 
   name: 'profile',
+
+  beforeCreate () {
+    document.querySelector('body').setAttribute('style', 'background: #FED6D6')
+  },
+  beforeDestroy () {
+    document.querySelector('body').setAttribute('style', 'background: #FED6D6')
+  },
 
   mounted: function () {
     console.log(this.$store.state.user);
@@ -45,5 +63,65 @@ export default {
 }
 </script>
 
+
 <style scoped>
-</style>>
+
+
+@import url('https://fonts.googleapis.com/css2?family=Anton&family=Roboto&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Anton&family=Libre+Baskerville:ital@1&family=Roboto&display=swap');
+
+.card {
+
+  display: flex;
+}
+
+.card__presentation {
+
+  color: black;
+  height: 100%;
+  width: 100%;
+  padding: 50px;
+  background: #FED6D6;
+}
+
+.groupamania {
+
+  font-family: 'Anton';
+}
+
+.step {
+  font-family: 'Anton';
+}
+
+.step h3 {
+
+  font-size: 20px;
+}
+
+.step__content {
+
+  font-family: 'Libre Baskerville';
+
+}
+
+button {
+
+  color: black;
+  border: #FED6D6 solid 2px;
+  background-color: white;
+
+}
+
+.card__content {
+}
+
+h1 {
+  font-size: 25px;
+}
+
+body {
+
+  background: black;
+}
+
+</style>
